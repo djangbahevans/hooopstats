62c50c9a800cd6320b9f2afbcfd4863d83f70bae
Evans Djangbah <djangbahevans@yahoo.com>
2024-05-15 15:20:24 +0000

    feat: Add migration file to create stats table

---
+++ migrations/20240515150848_create_stats_table.sql
@@ -0,0 +1,35 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    team_id INT NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES teams(id)
+);
+
+CREATE TABLE stats (
+    id SERIAL PRIMARY KEY,
+    points INT NOT NULL,
+    rebounds INT NOT NULL,
+    assists INT NOT NULL,
+    steals INT NOT NULL,
+    blocks INT NOT NULL,
+    fouls INT NOT NULL,
+    turnovers INT NOT NULL,
+    minutes_played INT NOT NULL,
+    player_id INT NOT NULL,
+    FOREIGN KEY (player_id) REFERENCES players(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE stats;
+DROP TABLE players;
+DROP TABLE teams;
+-- +goose StatementEnd
